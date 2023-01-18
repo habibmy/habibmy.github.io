65732822fc5120da821a6623466d588ff6f13fe0
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2023-01-18 13:20:46 +0530

    feat: .env and readme

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const username = 'aumirza'
+const username = process.env.REACT_APP_GITHUB_USERNAME
 export const reposUrl = `https://api.github.com/users/${username}/repos`
 export const profileUrl = `https://api.github.com/users/${username}`
 
